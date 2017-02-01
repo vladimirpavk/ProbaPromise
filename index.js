e882d58ce4e1bed1e14e975c7a3115a84267c090
Vladimir Pavković <vladimirpavk@gmail.com>
2017-02-01 09:18:20 +0100

    Updated...

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ let countPromises = 0;
 function button1_Click(){
    console.log('Dugme pritisnuto');
 
-   var p1=new Promise(
+   for(var x=0; x<50; x++){
+       let promise=new Promise(function(resolve, reject){
+           resolve(Math.random());
+       }).then(promiseResolved);
+   }
+
+   /*var p1=new Promise(
        function(resolve, reject){
 
             window.setInterval(function(){
                 document.getElementById("label1").innerHTML=Math.random();
             }, 3000);
-    });
+    });*/
+}
 
-
-
-
+function promiseResolved(val){  
+    console.log(val);
 }
